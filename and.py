1488fc6fedb6ba07ecd2dd99bde1382b8b8d8002
md nazmul islam <md.nazmul.islam0087@example.com>
2025-05-29 21:41:06 -0400

    utils and and file done

---
+++ and.py
@@ -0,0 +1,24 @@
+from utils.model import Perceptron
+from utils.all_utils import prepare_data
+import pandas as pd
+import numpy as np
+
+
+AND = {
+    "x1": [0,0,1,1],
+    "x2": [0,1,0,1],
+    "y": [0,0,0,1],
+}
+
+df = pd.DataFrame(AND)
+
+df
+X,y = prepare_data(df)
+
+ETA = 0.3 # 0 and 1
+EPOCHS = 10
+
+model_AND = Perceptron(eta=ETA, epochs=EPOCHS)
+model_AND.fit(X, y)
+
+_ = model_AND.total_loss()
