730223af925e71c44c6f9bf2bb04f45d904b2fa4
Islam <mislam41@kent.edu>
2025-06-17 15:13:06 -0400

    Logging module, docstring and TQDM added in xor.py

--- xor.py
+++ xor.py
@@ -2,13 +2,23 @@ from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_model,save_plot
 import pandas as pd
 import numpy as np
+import logging
+import os
+
+logging_str = "[%(asctime)s :  %(levelname)s : %(module)s] : %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)  # Create log directory if it doesn't exist
+logging.basicConfig(filename=os.path.join(log_dir, 'running_log.log'),
+                    filemode='a',  # 'w' to overwrite the log file, 'a' to append
+                    format=logging_str,
+                    level=logging.INFO)
 
 def main(data, eta, epochs, filename, plotFileName):
 
 
     df = pd.DataFrame(data)
 
-    print(df)
+    logging.info(f" XOR data frame{df}")
 
     X,y = prepare_data(df)
 
@@ -29,4 +39,12 @@ if __name__ == "__main__":
     
     ETA = 0.3 # 0 and 1
     EPOCHS = 10
-    main(data =XOR, eta=0.3, epochs=10, filename="xor.model", plotFileName="xor.png")
+    try:
+        logging.info(">>>>>>Starting XOR gate model training...>>>>>>>>")
+        main(data =XOR, eta=0.3, epochs=10, filename="xor.model", plotFileName="xor.png")
+        logging.info("<<<<<<<<<<XOR gate model training completed successfully.<<<<<<< \n")
+    except Exception as e :
+        logging.exception(e)
+        raise e
+    
+
