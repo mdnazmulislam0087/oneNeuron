c85f191cda9119138ffae8b4fe5bf149f1f0c2d3
Islam <mislam41@kent.edu>
2025-06-17 15:01:30 -0400

    Logging module, docstring and TQDM added in and.py

--- and.py
+++ and.py
@@ -2,13 +2,26 @@ from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_model,save_plot
 import pandas as pd
 import numpy as np
+import logging
+import os
+
+logging_str = "[%(asctime)s :  %(levelname)s : %(module)s] : %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)  # Create log directory if it doesn't exist
+logging.basicConfig(filename=os.path.join(log_dir, 'running_log.log'),
+    filemode='a',  # 'w' to overwrite the log file, 'a' to append
+    format=logging_str,
+    level=logging.INFO,
+    
+)
+
 
 def main(data, eta, epochs, filename, plotFileName):
 
 
     df = pd.DataFrame(data)
 
-    print(df)
+    logging.info(f"This is and Dataframe {df}")
 
     X,y = prepare_data(df)
 
@@ -29,4 +42,10 @@ if __name__ == "__main__":
     
     ETA = 0.3 # 0 and 1
     EPOCHS = 10
-    main(data =AND, eta=0.3, epochs=10, filename="and.model", plotFileName="and.png")
+    try:
+        logging.info(">>>>>>Starting AND gate model training...>>>>>>>>")
+        main(data =AND, eta=0.3, epochs=10, filename="and.model", plotFileName="and.png")
+        logging.info("<<<<<<<<<<AND gate model training completed successfully.<<<<<<< \n")
+    except Exception as e :
+        logging.exception(e)
+        raise e
